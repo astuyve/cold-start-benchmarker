72ebd0334aad1adf0379c9177bf02995f3c1e210
AJ Stuyvenberg <astuyve@gmail.com>
2023-10-23 14:49:06 -0400

    feat: embedded CW metrics support

--- handler.js
+++ handler.js
@@ -1,13 +1,17 @@
 const { LambdaClient, InvokeCommand, UpdateFunctionConfigurationCommand } = require("@aws-sdk/client-lambda")
 const lambdaClient = new LambdaClient({ maxAttempts: 10 })
+const { createMetricsLogger, Unit, StorageResolution } = require("aws-embedded-metrics");
 
 const invokeFunctions = async (lambdaFunctions, coldStart) => {
   return await Promise.all(lambdaFunctions.map((functionName) => invokeLambdaFunction(functionName, coldStart))) // invoke the functions
 }
 
+const delay = async (millis) => {
+  return new Promise((result) => setTimeout(result, millis ))
+}
+
 const invokeLambdaFunction = async (lambdaFunction, coldStart) => {
   if (coldStart) {
-    console.log('triggering cold start')
     await lambdaClient.send(
       new UpdateFunctionConfigurationCommand({
         FunctionName: lambdaFunction,
@@ -18,6 +22,7 @@ const invokeLambdaFunction = async (lambdaFunction, coldStart) => {
         },
       })
     )
+    await delay(200)
   }
   const start = Date.now()
   const { Payload } = await lambdaClient.send(
@@ -28,11 +33,17 @@ const invokeLambdaFunction = async (lambdaFunction, coldStart) => {
   )
   const finished = Date.now()
   const res = Buffer.from(Payload).toString('utf8')
-  const parsed = JSON.parse(res)
-  console.log(lambdaFunction, parsed)
-  const bodyParsed = JSON.parse(parsed.body)
-  const { handlerTs } = bodyParsed
-  const requestToHandlerMs = handlerTs - start
+  let requestToHandlerMs
+  try {
+    const parsed = JSON.parse(res)
+    console.log(lambdaFunction, parsed)
+    const bodyParsed = JSON.parse(parsed.body)
+    const { handlerTs } = bodyParsed
+    requestToHandlerMs = handlerTs - start
+  } catch {
+    console.log('Error parsing', res)
+    requestToHandlerMs = finished
+  }
   const rtt = finished - start
   return {
     functionName: lambdaFunction,
@@ -42,15 +53,14 @@ const invokeLambdaFunction = async (lambdaFunction, coldStart) => {
 }
 
 module.exports.invoke = async (event, context) => {
-  console.log(event.body)
-  const { functionNames, coldStart } = JSON.parse(event.body)
+  const metrics = createMetricsLogger();
+  const functionNames = process.env.FUNCTION_NAMES.split(', ')
+  const coldStart = true
   const results = await invokeFunctions(functionNames, coldStart)
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      coldStart,
-      results
-    })
-  }
-  return response
+  results.map((res) => {
+    metrics.putMetric(`${res.functionName}-rtt-latency`, res.roundTripInvokeMs, Unit.Milliseconds, StorageResolution.Standard);
+  })
+  await metrics.flush()
+  console.log('done')
+  return true
 }
